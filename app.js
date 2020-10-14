851201c14b79798002f252e038a015dfeebc9030
NEONSik <47497875+NEONSik@users.noreply.github.com>
2020-10-14 08:15:37 -0700

    fix: config deploy

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/digitalfaculty-front/index.html'));
 });
 
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
