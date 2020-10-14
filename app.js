403e73e17445a0e19a0fbf8d6681a70c51c66dc7
NEONSik <47497875+NEONSik@users.noreply.github.com>
2020-10-14 07:54:13 -0700

    fix:deploy config

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const compression = require('compression');
+
+const app = express();
+
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, 'dist/digitalfaculty-front')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/digitalfaculty-front/index.html'));
+});
+
+const port = process.env.PORT || 4200;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log("running"));
