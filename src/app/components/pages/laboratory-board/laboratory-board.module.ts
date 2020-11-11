9b0beed588daaf9934680673d6d1ba777ce719f9
b1ood <71218071+b1ood@users.noreply.github.com>
2020-11-11 14:04:33 +0300

    Added header on the lab-page

--- src/app/components/pages/laboratory-board/laboratory-board.module.ts
+++ src/app/components/pages/laboratory-board/laboratory-board.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LaboratoryBoardComponent } from './laboratory-board.component';
+import {HeaderModule} from "../../../core/header/header.module";
 
 
 
 @NgModule({
   declarations: [LaboratoryBoardComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        HeaderModule
+    ]
 })
 export class LaboratoryBoardModule { }
