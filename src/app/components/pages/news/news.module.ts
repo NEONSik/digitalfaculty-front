dc282f91aa6813f83cb13e1bed3770c5c2b7b572
b1ood <71218071+b1ood@users.noreply.github.com>
2020-11-20 15:38:19 +0300

    Created a news-page

--- src/app/components/pages/news/news.module.ts
+++ src/app/components/pages/news/news.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsComponent } from './news.component';
+import {HeaderModule} from "../../../core/header/header.module";
 
 
 
 @NgModule({
   declarations: [NewsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HeaderModule
   ]
 })
 export class NewsModule { }
