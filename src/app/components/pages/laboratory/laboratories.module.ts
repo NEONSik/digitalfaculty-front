a8e68524f224fbf6622709d2a5687a4affdbd95d
b1ood <71218071+b1ood@users.noreply.github.com>
2020-12-01 17:21:48 +0300

    fixed lab-page

--- src/app/components/pages/laboratory/laboratories.module.ts
+++ src/app/components/pages/laboratory/laboratories.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LaboratoriesComponent} from './laboratories.component';
 import {RouterModule} from '@angular/router';
+import {HeaderModule} from "../../../core/header/header.module";
 
 
 @NgModule({
   declarations: [LaboratoriesComponent],
   exports: [LaboratoriesComponent],
   imports: [
+    HeaderModule,
     CommonModule,
     RouterModule
   ]
